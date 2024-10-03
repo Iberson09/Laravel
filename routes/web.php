387eb065d06271950d7cae76b9d81352c0102110
Jhon Iberson Mariñas <96397779+Iberson09@users.noreply.github.com>
2024-10-03 05:53:23 +0000

    Laravel Login and Register

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\URL;
-
-$url = config('app.url');
-URL::forceRootUrl($url);
 
 Route::get('/', function () {
     return view('welcome');
